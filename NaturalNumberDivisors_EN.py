e2261ec6964e1fa1617556a7464352f957019d11
Alex Tretyakov <tretyakov-a.n@yandex.ru>
2017-03-30 23:31:35 +0300

    Create NaturalNumberDivisors_EN.py

---
+++ NaturalNumberDivisors_EN.py
@@ -0,0 +1,12 @@
+#calculate the number of natural divisors of the number x
+#(include 1 and the number itself; x <= 30000)
+x = int(input('Enter the number x (x <= 30000): '))     #input number x
+def f(x):                                               
+    n = 0                                               
+    print('All natural divisors of a number x:')        #output of all natural divisors
+    for i in range(1, x + 1):                           
+        if (x % i == 0):                                #для всех нат.дел-й
+            n += 1                                      #calculate dividers
+            print(i)                                    #output divisors
+    print('Total natural divisors of the number x:', n) 
+f(x)                                                    #function call
