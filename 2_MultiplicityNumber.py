75f71f62889016a90aad8a1a8e38c0ca8e98afae
Alex Tretyakov <tretyakov-a.n@yandex.ru>
2017-03-31 18:19:24 +0300

    Update 2_MultiplicityNumber.py

--- 2_MultiplicityNumber.py
+++ 2_MultiplicityNumber.py
@@ -1,7 +1,10 @@
 #2_MultiplicityNumber.py
 #given a number, output True if it is a multiple of 3 and not a multiple of 5
 i = int(input('Enter integer data: '))
-if i % 3 == 0 and i % 5 != 0:                               #checking the conditions
-    print('True. Введённое число кратно 3 и не кратно 5')   #output if True
+#checking the conditions
+if i % 3 == 0 and i % 5 != 0:
+    #output if True
+    print('True. The number entered is a multiple of 3 and not a multiple of 5')
 else:
-    print('False')                                          #output if False
+    #output if False
+    print('False')
