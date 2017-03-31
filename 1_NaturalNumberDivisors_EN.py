7027a1d10651678945fb5bc135a3c76326b9fcf1
Alex Tretyakov <tretyakov-a.n@yandex.ru>
2017-03-31 18:15:03 +0300

    Update and rename NaturalNumberDivisors_EN.py to 1_NaturalNumberDivisors_EN.py

--- 1_NaturalNumberDivisors_EN.py
+++ 1_NaturalNumberDivisors_EN.py
@@ -5,8 +5,8 @@ def f(x):
     n = 0                                               
     print('All natural divisors of a number x:')        #output of all natural divisors
     for i in range(1, x + 1):                           
-        if (x % i == 0):                                #для всех нат.дел-й
-            n += 1                                      #calculate dividers
+        if (x % i == 0):                                #for all natural divisors
+            n += 1                                      #calculate divisors
             print(i)                                    #output divisors
     print('Total natural divisors of the number x:', n) 
 f(x)                                                    #function call
